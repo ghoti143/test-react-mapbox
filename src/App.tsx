572d1c22156cfd686826dd878dbdd8fedb7fd4a1
Mike Haggerty <mike.haggerty@airspacelink.com>
2021-07-17 13:19:29 -0400

    foo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,62 @@
-import React, { useEffect, useState } from "react";
-import "./index.css";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZ2hvdGkxNDMiLCJhIjoiY2tyN3pyeG5pMjlmbDMwdGM4cXd0Ym9rZyJ9.o1jhOzwF0s07Aaq5WrS-_Q";
 
 export default () => {
-  var foo: number = 0;
-  const [count, setCount] = useState(foo);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
-  // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+    map.current.on("load", () => {
+      // This code runs once the base style has finished loading.
+
+      map.current.addSource("airports", {
+        type: "geojson",
+        data: "https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/ArcGIS/rest/services/US_Airport/FeatureServer/0/query?f=pgeojson&where=1=1",
+      });
+
+      map.current.addLayer({
+        id: "airports-circle",
+        type: "circle",
+        source: "airports",
+
+        paint: {
+          "circle-color": "hsla(0,0%,0%,0.75)",
+          "circle-stroke-width": 1.5,
+          "circle-stroke-color": "white",
+        },
+      });
+    });
   });
 
   return (
-    <div>
-      <p>You clicked {count} times you beast</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+    <div id="app">
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 };
